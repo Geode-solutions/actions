2f8688b01f299ca81ac806656da32e2133297a2f
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-03 14:17:36 +0200

    Update entrypoint.sh

--- clang-format/entrypoint.sh
+++ clang-format/entrypoint.sh
@@ -2,12 +2,11 @@
 set -e
 
 cd "${GITHUB_WORKSPACE}"
-wget https://raw.githubusercontent.com/Geode-solutions/actions/master/clang-format/.clang-format -O .clang-formatœ
+wget https://raw.githubusercontent.com/Geode-solutions/actions/master/clang-format/.clang-format -O .clang-format
 
 files=$(find . \( -name "*.h" -o -name "*.cpp" \))
 clang-format-8 -style=file -i $files
 
-if [ -n $(git diff) ]
-then 
+if [[ `git status --porcelain` ]]; then
     echo ::error::Format requirement is not satisfied
 fi

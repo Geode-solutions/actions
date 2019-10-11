006dceb1e0b2a395a99997f67d02f286097ca551
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-11 14:57:54 +0200

    Update entrypoint.sh

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-third_party_path="${GITHUB_WORKSPACE}/doxyrest"
+third_party_path="$GITHUB_WORKSPACE/doxyrest"
 mkdir -p $third_party_path
 cd $third_party_path
 doxyrest_name="doxyrest-2.0.0-linux-amd64"
@@ -9,8 +9,8 @@ wget https://github.com/vovkos/doxyrest/releases/download/doxyrest-2.0.0/$doxyre
 tar -xJf $doxyrest_name.tar.xz
 doxyrest_path="$third_party_path/$doxyrest_name"
 
-cd "${GITHUB_WORKSPACE}/$INPUT_DIRECTORY"
-cmake ${GITHUB_WORKSPACE}
+cd "$GITHUB_WORKSPACE/$INPUT_DIRECTORY"
+cmake .
 make doc
 cd doc
 $doxyrest_path/bin/doxyrest xml/index.xml -c $doxyrest_path/share/doxyrest/frame/doxyrest-config.lua -F $doxyrest_path/share/doxyrest/frame/common -F $doxyrest_path/share/doxyrest/frame/cfamily 
@@ -25,9 +25,9 @@ rm -rf $docs_path/static/docs/$repo_name
 mkdir -p $docs_path/static/docs/$repo_name
 cp -r ./site/* $docs_path/static/docs/$repo_name
 cd $docs_path
-git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
-git config user.name ${GITHUB_ACTOR}
+git config user.email $GITHUB_ACTOR@users.noreply.github.com
+git config user.name $GITHUB_ACTOR
 git checkout master
 git add --all
 git commit -m "Update $repo_name documentation"
-git push https://BotellaA:${GITHUB_TOKEN}@github.com/Geode-solutions/docs
+git push https://BotellaA:$GITHUB_TOKEN@github.com/Geode-solutions/docs

a7613e2788a426c835c43207b7132ca9de785317
BotellaA <arnaud.botella@geode-solutions.com>
2019-10-22 10:45:50 +0200

    add logging

--- get-release/index.js
+++ get-release/index.js
@@ -7,6 +7,7 @@ const tar = require('tar');
 
 try {
   const repo = core.getInput('repository', { required: true });
+  console.log("Looking for repository:", repo);
   const owner = 'Geode-solutions';
   const file = core.getInput('file', { required: true });
   const outputFile = repo.concat(file);
@@ -25,6 +26,7 @@ try {
       octokit.repos.listAssetsForRelease({owner,repo,release_id})
         .then(assets => {
           const asset = assets.data.find(asset => asset.name.includes(file));
+          console.log("Asset name:", asset.name);
           let assetUrl = asset.url
           if (hasToken){
             assetUrl = assetUrl.concat('?access_token=', token);
@@ -34,16 +36,19 @@ try {
               accept: 'application/octet-stream'
             }
           }).then(response => {
+            console.log("Downloading to:", outputFile);
             response.body.pipe(fs.createWriteStream(outputFile)).on('finish', function() {
               const archive = fs.createReadStream(outputFile);
               const extension = outputFile.split('.').pop();
               if (extension == "zip"){
                 archive.pipe(unzipper.Extract({ path: '.' })).on('finish', function() {
+                  console.log("Unzip to:", asset.name.slice(0,-4));
                   core.setOutput('path', asset.name.slice(0,-4));
                   fs.unlinkSync(outputFile);
                 });
               } else if (extension == "gz"){
                 archive.pipe(tar.x()).on('finish', function() {
+                  console.log("Untar to:", asset.name.slice(0,-7));
                   core.setOutput('path', asset.name.slice(0,-7));
                   fs.unlinkSync(outputFile);
                 });

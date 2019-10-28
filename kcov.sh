db21d8ea57c58516f8f35d56a8fcce58ff8dbd0f
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-28 15:55:45 +0100

    Update kcov.sh

--- kcov.sh
+++ kcov.sh
@@ -19,11 +19,9 @@ do
     kcov --include-path="${GITHUB_WORKSPACE}/include","${GITHUB_WORKSPACE}/src" --exclude-pattern=common.cpp $OUTPUT_DIRECTORY $test
 done
 
-ls $OUTPUT_DIRECTORY
-TEST_PATH="$(ls -d $OUTPUT_DIRECTORY/*/)"
-TEST_PATH2="$(find -type d -mindepth 1 -maxdepth 1  $OUTPUT_DIRECTORY/test-*)"
-NB_TESTS="$(find $OUTPUT_DIRECTORY/test-* | wc -l)"
-echo $TEST_NAME
+TEST_PATH="$(find $OUTPUT_DIRECTORY/test-* -maxdepth 0 -type d)"
+echo $TEST_PATH
+NB_TESTS="$(find $1/test-* | wc -l)"
 echo $NB_TESTS
 if [ ${NB_TESTS} -eq "1" ]; then 
     cp $TEST_PATH/cobertura.xml $OUTPUT_DIRECTORY/kcov-merged;

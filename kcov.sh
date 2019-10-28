4cd183b2cefa62e00bceecc7e87bf69e4415a637
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-28 15:47:30 +0100

    Update kcov.sh

--- kcov.sh
+++ kcov.sh
@@ -19,8 +19,10 @@ do
     kcov --include-path="${GITHUB_WORKSPACE}/include","${GITHUB_WORKSPACE}/src" --exclude-pattern=common.cpp $OUTPUT_DIRECTORY $test
 done
 
+ls $OUTPUT_DIRECTORY
 TEST_PATH="$(ls -d $OUTPUT_DIRECTORY/*/)"
-NB_TESTS="$(find $1/test-* | wc -l)"
+TEST_PATH2="$(find -type d -mindepth 1 -maxdepth 1  $OUTPUT_DIRECTORY/*/)"
+NB_TESTS="$(find $OUTPUT_DIRECTORY/test-* | wc -l)"
 echo $TEST_NAME
 echo $NB_TESTS
 if [ ${NB_TESTS} -eq "1" ]; then 

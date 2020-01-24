eb33051753949a8db33bb6f7f98759d6da8b41b9
Arnaud Botella <arnaud.botella@geode-solutions.com>
2020-01-24 14:55:36 +0100

    Update entrypoint.sh

--- clang-format/entrypoint.sh
+++ clang-format/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 wget https://raw.githubusercontent.com/Geode-solutions/actions/master/clang-format/.clang-format -O .clang-format
 
-git checkout `echo ${GITHUB_REF#*/*/}`
 files=$(find . \( -name "*.h" -o -name "*.cpp" \))
 clang-format-8 -style=file -i $files
 
@@ -11,9 +10,11 @@ changes=$(git diff)
 if [ -n "$changes" ]; then
     git config user.email $GITHUB_ACTOR@users.noreply.github.com
     git config user.name $GITHUB_ACTOR
+    branch=`echo ${GITHUB_REF#*/*/}`
+    git checkout $branch
     git add --all
     git commit -m "style: CI format update"
-    git push https://BotellaA:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+    git push https://BotellaA:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY HEAD:$branch
     echo ::error::Format requirement is not satisfied
     exit 1
 fi

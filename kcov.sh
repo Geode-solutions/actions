99ff4dedbefcb3afaa07e37c23d00afdd01e933a
Arnaud Botella <arnaud.botella@geode-solutions.com>
2020-01-30 09:39:39 +0100

    Update kcov.sh

--- kcov.sh
+++ kcov.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo apt update
-sudo apt install pkg-config binutils-dev build-essential libcurl4-openssl-dev libdw-dev libiberty-dev python zlib1g-dev
-        
-git clone https://github.com/SimonKagstrom/kcov kcov_src
-cd kcov_src
-cmake .
-cmake --build . -- -j2
-sudo cmake --build . --target install
-
 OUTPUT_DIRECTORY="${GITHUB_WORKSPACE}/kcov"
 mkdir -p $OUTPUT_DIRECTORY
 cd $1

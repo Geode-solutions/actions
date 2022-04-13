547878488b929dee2ca4b0e30babc60c469c52f3
Arnaud Botella <arnaud.botella@geode-solutions.com>
2022-04-13 10:39:02 +0200

    Update entrypoint.sh

--- clang-format/entrypoint.sh
+++ clang-format/entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 wget https://raw.githubusercontent.com/Geode-solutions/actions/master/clang-format/.clang-format -O .clang-format
-pwd
-ls -a
+
+ls .git
+git --version
 files=$(find . \( -name "*.h" -o -name "*.cpp" \))
 clang-format-12 -style=file -i $files
 

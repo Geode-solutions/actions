e9c2030c65a6c28c61fa3c17863ce967ede119ba
Arnaud Botella <@users.noreply.github.com>
2022-06-15 00:05:36 +0200

    test

--- versions/commit.sh
+++ versions/commit.sh
@@ -7,11 +7,7 @@ if [ -n "$changes" ]; then
     git config user.email $GITHUB_ACTOR@users.noreply.github.com
     git config user.name $GITHUB_ACTOR
     branch=`echo ${GITHUB_REF#*/*/}`
-    echo $branch
-    git checkout $branch
     git add --all
     git commit -m "Update versions"
-    git push https://$1:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY HEAD:$branch
-    echo ::error::Format requirement is not satisfied
-    exit 1
+    git push https://BotellaA:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
 fi
--- versions/index.js
+++ versions/index.js
@@ -3,6 +3,7 @@ const {Octokit} = require('@octokit/rest');
 const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
+const {exit} = require('process');
 
 try {
   const repos = core.getInput('repository');
@@ -15,7 +16,7 @@ try {
       if (!repo.length) {
         return;
       }
-      let promise = new Promise(function(resolve) {
+      let promise = new Promise(function(resolve, reject) {
         console.log('Looking for repository:', repo);
         octokit.rest.actions
             .listWorkflowRunsForRepo(
@@ -23,27 +24,31 @@ try {
             .then(wf => {
               const conclusion = wf.data.workflow_runs[0].conclusion;
               console.log(conclusion);
-              if (conclusion == 'success') {
-                octokit.repos.getLatestRelease({owner, repo}).then(release => {
-                  const version = release.data.tag_name;
-                  console.log(release.data.tag_name);
-                  resolve({repo, version})
-                })
+              if (conclusion != 'success') {
+                reject('Last schedule does not work:' + repo);
               }
+              octokit.repos.getLatestRelease({owner, repo}).then(release => {
+                const version = release.data.tag_name;
+                console.log(release.data.tag_name);
+                resolve({repo, version})
+              })
             });
       });
       results.push(promise);
     });
-    Promise.all(results).then((outputs) => {
-      console.log(outputs)
-      const json = JSON.stringify(outputs.sort(), null, 2)
-      console.log(json);
-      fs.writeFile('versions.json', json, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-        child_process.execSync(path.join(__dirname, 'commit.sh'))
-      })
-    })
+    Promise.all(results)
+        .then((outputs) => {
+          const json = JSON.stringify(outputs.sort(), null, 2)
+          console.log(json);
+          fs.writeFile('versions.json', json, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            const log =
+                child_process.execSync(path.join(__dirname, 'commit.sh'))
+            console.log(log.toString())
+          })
+        })
+        .catch(error => core.setFailed(error))
   }
 } catch (error) {
   core.setFailed(error.message);

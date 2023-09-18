11042ad79ac465c4b4a0d2197f1508243c467ea0
Arnaud Botella <@users.noreply.github.com>
2023-09-18 12:00:14 +0200

    test

--- doc/style.py
+++ doc/style.py
@@ -39,6 +39,10 @@ def sort(stream, output, directory):
             output.write("```cpp\n")
             output.write(stripped[1:-2])
             output.write("\n```\n")
+        elif stripped[:7] == "public ":
+            output.write("```cpp\n")
+            output.write(line)
+            output.write("```\n")
         elif stripped[0] == "#" and "<" in stripped and ">" in stripped:
             output.write(stripped.split("<", 1)[0])
             output.write("\n")

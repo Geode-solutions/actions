ef8aaccad43ec89f1c1dcd422fef5639e7bf499c
Arnaud Botella <@users.noreply.github.com>
2023-09-18 09:57:02 +0200

    test

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
+pwd
 cd build/$INPUT_DIRECTORY
+ls
+head compile_commands.json
 clang-doc-18  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
 cd doc/geode
 python3 /style.py .

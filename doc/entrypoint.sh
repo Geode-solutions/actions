4ef79ab4204aad40d302652a03a2e6084c1e2447
Arnaud Botella <@users.noreply.github.com>
2023-09-18 09:50:54 +0200

    clang-doc-18

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-clang-doc --version
 cd build/$INPUT_DIRECTORY
-clang-doc  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
+clang-doc-18  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
+cd doc/geode
 python3 /style.py .

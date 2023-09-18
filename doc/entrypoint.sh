b5dc0f8cb5240e739234d585325a808147faa799
Arnaud Botella <@users.noreply.github.com>
2023-09-18 09:32:49 +0200

    test

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-ls 
-cd $1
-clang-doc-16  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
-python3 style.py .
+clang-doc --version
+cd build/$INPUT_DIRECTORY
+clang-doc  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
+python3 /style.py .

7861f773356505060681e75f0348da898b860959
Arnaud Botella <@users.noreply.github.com>
2023-09-18 10:55:36 +0200

    test

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -15,19 +15,20 @@ cd doc/geode
 python3 /style.py .
 
 git clone https://github.com/Geode-solutions/docs $docs_path
+git remote add origin git@github.com:BotellaA/Geode-solutions/docs.git
+cd $docs_path
+git checkout master
+git pull 
 doc_folder=$docs_path/references/$repo_name
 rm -rf $doc_folder
 mkdir -p $doc_folder
-cp -r . $doc_folder
-cd $docs_path
-
+cp -r /github/workspace/build/$directory/doc/geode/* $doc_folder
 
 changes=$(git status --porcelain 2>/dev/null | wc -l)
 if [ "$changes" -ne "0" ]; then
     git config user.email $GITHUB_ACTOR@users.noreply.github.com
-    git config user.name $GITHUB_ACTOR
-    git checkout master
+    git config user.name $GITHUB_ACTOR    
     git add --all
     git commit -m "Update $repo_name documentation"
-    git push https://BotellaA:$token@github.com/Geode-solutions/docs2
+    git push origin master
 fi

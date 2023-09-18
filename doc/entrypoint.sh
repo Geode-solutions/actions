ed31d465fd0a970c19b7eb784874c26be3ee97c0
Arnaud Botella <@users.noreply.github.com>
2023-09-18 10:23:11 +0200

    test

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -l
 
+directory = $1
+base_directory = $2
 pwd
-echo "base=$INPUT_BASE_DIRECTORY"
-cd build/$INPUT_DIRECTORY
+echo "base=$base_directory"
+cd build/$directory
 ls
 head compile_commands.json
-sed -i -E  "s~$INPUT_BASE_DIRECTORY~/github/workspace~g" compile_commands.json
+sed -i -E "s~$base_directory~/github/workspace~g" compile_commands.json
 head compile_commands.json
 clang-doc-18  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
 cd doc/geode

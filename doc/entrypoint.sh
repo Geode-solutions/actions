0cba44e8fc50a76ae791393dd07dd6e80cd467c0
Arnaud Botella <@users.noreply.github.com>
2023-09-18 10:10:32 +0200

    test g

--- doc/entrypoint.sh
+++ doc/entrypoint.sh
@@ -5,7 +5,7 @@ echo $INPUT_BASE_DIRECTORY
 cd build/$INPUT_DIRECTORY
 ls
 head compile_commands.json
-sed -i -E  "s/$INPUT_BASE_DIRECTORY/\/github\/workspace/" compile_commands.json
+sed -i -E  "s/$INPUT_BASE_DIRECTORY/\/github\/workspace/g" compile_commands.json
 head compile_commands.json
 clang-doc-18  --output=doc --doxygen --public --format=md --executor=all-TUs compile_commands.json
 cd doc/geode

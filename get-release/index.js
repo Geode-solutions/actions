006f909b783339a304df625c5b0bfd88416f25b8
Arnaud Botella <@users.noreply.github.com>
2023-11-30 14:20:04 +0100

    typeof

--- get-release/index.js
+++ get-release/index.js
@@ -60,7 +60,7 @@ const main = async () => {
                   asset.name.includes(file)
                 )
                 console.log("Asset:", asset)
-                if (asset == undefined) {
+                if (typeof asset === undefined) {
                   resolve("")
                 }
                 console.log("Asset name:", asset.name)

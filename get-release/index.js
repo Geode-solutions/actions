a556b7915585f4f569b3782dcd89a23c098d79eb
Arnaud Botella <@users.noreply.github.com>
2023-12-05 09:16:25 +0100

    less log

--- get-release/index.js
+++ get-release/index.js
@@ -29,7 +29,6 @@ const main = async () => {
         }
         let promise = new Promise(function (resolve) {
           console.log("Looking for repository:", repo)
-          console.log(github.context.ref, branch, github.context.ref.includes(branch));
           const query = branch.includes("master")
             ? octokit.repos
                 .getLatestRelease({ owner, repo })
@@ -45,8 +44,7 @@ const main = async () => {
                   }
                 }
                 const branch_release = releases.data.find(
-                  (r) => {console.log(r.name)
-                    return r.name === branch}
+                  (r) =>  r.name === branch
                 )
                 if (branch_release) {
                   return branch_release.id

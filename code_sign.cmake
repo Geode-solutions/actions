40d2415ef42cd471924cddd395bd54cad2e8d9dd
Arnaud Botella <@users.noreply.github.com>
2024-01-18 15:58:24 +0100

    test

--- code_sign.cmake
+++ code_sign.cmake
@@ -1,11 +1,11 @@
 cmake_minimum_required(VERSION 3.12)
 
 set(SIGN_COMMAND "AzureSignTool sign \
---azure-key-vault-url \"$ENV{ AZURE_KEY_VAULT_URI }\" \
---azure-key-vault-client-id \"$ENV{ AZURE_CLIENT_ID }\" \
---azure-key-vault-tenant-id \"$ENV{ AZURE_TENANT_ID }\" \
---azure-key-vault-client-secret \"$ENV{ AZURE_CLIENT_SECRET }\" \
---azure-key-vault-certificate $ENV{ AZURE_CERT_NAME } \
+--azure-key-vault-url \"$ENV{AZURE_KEY_VAULT_URI}\" \
+--azure-key-vault-client-id \"$ENV{AZURE_CLIENT_ID}\" \
+--azure-key-vault-tenant-id \"$ENV{AZURE_TENANT_ID}\" \
+--azure-key-vault-client-secret \"$ENV{AZURE_CLIENT_SECRET}\" \
+--azure-key-vault-certificate $ENV{AZURE_CERT_NAME} \
 --timestamp-rfc3161 http://timestamp.digicert.com -v \
 ")
 message(STATUS "Sign command: ${SIGN_COMMAND}")

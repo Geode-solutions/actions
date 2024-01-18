b4398889af9a7d343cf0cde09a8793884a123d7a
Arnaud Botella <@users.noreply.github.com>
2024-01-18 16:34:00 +0100

    echo log

--- code_sign.cmake
+++ code_sign.cmake
@@ -21,12 +21,12 @@ foreach(DLL_FILE ${DLL_FILES})
     execute_process(
         COMMAND ${SIGN_COMMAND} ${DLL_FILE}
         RESULT_VARIABLE SIGN_RESULT
-        OUTPUT_VARIABLE SIGN_OUTPUT
+        ECHO_OUTPUT_VARIABLE
+        ECHO_ERROR_VARIABLE
     )
     if(SIGN_RESULT EQUAL 0)
         message(STATUS "Command executed successfully on ${DLL_FILE}")
     else()
         message(STATUS "Command failed on ${DLL_FILE}")
-        message(STATUS "Output: ${SIGN_OUTPUT}")
     endif()
 endforeach()

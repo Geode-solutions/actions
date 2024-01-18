f813290a7fe62721e2d82ae5cfe65ecf968a7243
Arnaud Botella <@users.noreply.github.com>
2024-01-18 20:23:29 +0100

    test

--- code_sign.cmake
+++ code_sign.cmake
@@ -1,6 +1,9 @@
 cmake_minimum_required(VERSION 3.18)
 
-set(SIGN_COMMAND "AzureSignTool sign \
+find_program(AZURE_SIGN_TOOL AzureSignTool REQUIRED)
+message(STATUS "AzureSignTool: ${AZURE_SIGN_TOOL}")
+
+set(SIGN_COMMAND "${AZURE_SIGN_TOOL} sign \
 --azure-key-vault-url \"$ENV{AZURE_KEY_VAULT_URI}\" \
 --azure-key-vault-client-id \"$ENV{AZURE_CLIENT_ID}\" \
 --azure-key-vault-tenant-id \"$ENV{AZURE_TENANT_ID}\" \
@@ -11,20 +14,7 @@ set(SIGN_COMMAND "AzureSignTool sign \
 message(STATUS "Sign command: ${SIGN_COMMAND}")
 message(STATUS "Temporary install directory: ${CPACK_TEMPORARY_INSTALL_DIRECTORY}")
 
-find_program(AZURE_SIGN_TOOL AzureSignTool REQUIRED)
-
-message(STATUS "AzureSignTool: ${AZURE_SIGN_TOOL}")
-
-execute_process(
-    COMMAND "AzureSignTool --help"
-    ECHO_OUTPUT_VARIABLE
-    ECHO_ERROR_VARIABLE
-)
-
 file(GLOB_RECURSE DLL_FILES "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/*.dll")
-message(STATUS "DLL files: ${DLL_FILES}")
-
-
 foreach(DLL_FILE ${DLL_FILES})
     message(STATUS "Signing ${DLL_FILE}")
     execute_process(

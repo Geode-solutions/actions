fdc534c3843f709420e584f4aecf7b5b86b13df7
Arnaud Botella <@users.noreply.github.com>
2024-07-01 20:36:38 +0200

    add actions from feflow

---
+++ get-caches/index.js
@@ -0,0 +1,34 @@
+const core = require("@actions/core");
+const cache = require("@actions/cache");
+const fs = require("fs");
+
+const main = async () => {
+  try {
+    const system = core.getInput("system");
+    console.log("system", system);
+    const keys = core.getInput("keys").split(";");
+    console.log("keys", keys);
+    if (keys.length == 0) {
+      return 0;
+    }
+    for (let key of keys) {
+      if (!key.length) {
+        return;
+      }
+      console.log("key", key);
+      const system_key = system + "-" + key;
+      console.log("Looking for cache:", system_key);
+      const cacheKey = await cache.restoreCache(["build/install"], system_key);
+      console.log("cacheKey", cacheKey);
+      if (!cacheKey) {
+        core.setFailed("Failed to retrieve cache for " + system_key);
+      }
+    }
+    fs.renameSync("build/install", "prefix");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+// Call the main function to run the action
+main();

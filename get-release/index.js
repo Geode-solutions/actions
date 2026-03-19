c15d30ac77b78d7645cbac156627fec01eb75263
Arnaud Botella <@users.noreply.github.com>
2026-03-19 10:21:11 +0100

    toString

--- get-release/index.js
+++ get-release/index.js
@@ -36,9 +36,10 @@ const download_asset = async (asset, token) => {
             const full = path.join(process.env.GITHUB_WORKSPACE, entry.name)
             const mode = (entry.attr >>> 16) >>> 0xffff
             const permissions = mode & 0o777
-            console.log({ full, permissions })
-            if (permissions) {
-              fs.chmodSync(full, permissions)
+            const readable_mode = permissions.toString(8)
+            console.log({ full, readable_mode })
+            if (readable_mode) {
+              fs.chmodSync(full, readable_mode)
             }
           }
           await zip.close()
